9c64c6e124c197cbe6bf81d167d0371adf803a43
Rahul Ranjan <rahul.ranjan7201@gmail.com>
2023-01-21 21:19:42 +0530

    Separting JS

---
+++ P1/App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("h1", { id:"title1"}, "Hello from React Class")
+const heading2 = React.createElement("h1", {
+    id:"title"
+}, "Hello from React Class 2")
+const container = React.createElement("div" , {
+    id:"container"
+}, [heading, heading2])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing the react element inside the root
+root.render(container)
