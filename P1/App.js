7d1bc9e1cdfc588cdfcec7f8364f29a3c5c11e0c
Rahul Ranjan <rahul.ranjan7201@gmail.com>
2023-01-29 16:33:11 +0530

    Adding JSX ways

--- P1/App.js
+++ P1/App.js
@@ -2,23 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 const heading = React.createElement(
   "h1",
-  { id: "title1" },
+  { id: "title1", key: "h1" },
   "Hello from React Class"
 );
-const heading2 = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Hello from React Class 2"
+console.log(heading);
+// JSX way
+const heading1 = (
+  <h1 id="heading" key="h2">
+    Hello React
+  </h1>
 );
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading2]
-);
+const HeadingComponent = () => {
+  return <h1>React Functional Component </h1>
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing the react element inside the root
-root.render(container);
+root.render(heading);
