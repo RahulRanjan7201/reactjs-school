1b0cbe869c28c8a9f64e28d28e4ce93dad7bb504
Rahul Ranjan <rahul.ranjan7201@gmail.com>
2023-01-30 16:51:21 +0530

    Nested header element using React.createElement

--- P1/App.js
+++ P1/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  { id: "title1", key: "h1" },
-  "Hello from React Class"
-);
-const Title =() => {
-  return <h1>My name is ... I forgot</h1>
-}
-const HeadingComponent = () => {
-  return (
-    <div>
-      {heading}
-      <Title/>
-      {Title()}
-      <h1>Hello</h1>
-      <h2>Bye</h2>
-    </div>
-  );
-};
+const header = React.createElement(
+  "div",
+  {className:"title"},
+  [React.createElement(
+    "h1", 
+    {},
+    "Heading H1" ),
+    React.createElement(
+      "h2",
+      {},
+      "Heading h2"
+    ),
+    React.createElement(
+      "h3",
+      {},
+      "Heading h3"
+    )
+  ] 
+
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing the react element inside the root
-root.render(<HeadingComponent/>);
+root.render(header);
