73c47c448365aad138da5e42d1fa824e71d6e75c
Rahul Ranjan <rahul.ranjan7201@gmail.com>
2023-01-30 16:21:39 +0530

    Class 3 Assignments

--- P1/App.js
+++ P1/App.js
@@ -5,16 +5,20 @@ const heading = React.createElement(
   { id: "title1", key: "h1" },
   "Hello from React Class"
 );
-console.log(heading);
-// JSX way
-const heading1 = (
-  <h1 id="heading" key="h2">
-    Hello React
-  </h1>
-);
-const HeadingComponent = () => {
-  return <h1>React Functional Component </h1>
+const Title =() => {
+  return <h1>My name is ... I forgot</h1>
 }
+const HeadingComponent = () => {
+  return (
+    <div>
+      {heading}
+      <Title/>
+      {Title()}
+      <h1>Hello</h1>
+      <h2>Bye</h2>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing the react element inside the root
-root.render(heading);
+root.render(<HeadingComponent/>);
