b81b0363509d818bafb5a0af50793b17c69cd0f0
Rahul Ranjan <rahul.ranjan7201@gmail.com>
2023-01-30 17:00:56 +0530

    JSX

--- P1/App.js
+++ P1/App.js
@@ -1,25 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const header = React.createElement(
-  "div",
-  {className:"title"},
-  [React.createElement(
-    "h1", 
-    {},
-    "Heading H1" ),
-    React.createElement(
-      "h2",
-      {},
-      "Heading h2"
-    ),
-    React.createElement(
-      "h3",
-      {},
-      "Heading h3"
-    )
-  ] 
-
+const header =(
+  <div className="title">
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+  </div>
 )
+ 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing the react element inside the root
 root.render(header);
