165d9832f5856404149cdf791bbc9b3395705d4d
Rahul Ranjan <rahul.ranjan7201@gmail.com>
2023-01-31 12:47:08 +0530

    Offer Villa

--- P1/App.js
+++ P1/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const header = () => {
- return  (
-  <div className="main">
-    <span>Icon</span>
-    <span>Search Bar </span>
-  </div>
+const Title = () => {
+  return  (
+  <h1 id="title" key="h1">
+    Offers Villa 
+  </h1>
 )
-  }
+}
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Title/>
+     <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+     </div>
+    </div>
+  )
+}
+
+const AppLayout = () => {
+  return {}
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing the react element inside the root
-root.render(header());
+root.render(<HeaderComponent/>);
