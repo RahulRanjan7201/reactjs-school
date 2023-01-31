9e2508529b4d315c389faa88bfc0aa5b682c9ea9
Rahul Ranjan <rahul.ranjan7201@gmail.com>
2023-01-31 12:46:57 +0530

    Offers Villa

--- P1/App.js
+++ P1/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const header =(
-  <div className="title">
-    <h1>Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
+const header = () => {
+ return  (
+  <div className="main">
+    <span>Icon</span>
+    <span>Search Bar </span>
   </div>
 )
- 
+  }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing the react element inside the root
-root.render(header);
+root.render(header());
