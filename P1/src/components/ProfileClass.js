02e058445025bb0224b4b8cbce9c16fca6aeda15
Rahul Ranjan <rahul.ranjan7201@gmail.com>
2023-02-20 21:31:27 +0530

    Chapter 8 complete

--- P1/src/components/ProfileClass.js
+++ P1/src/components/ProfileClass.js
@@ -7,28 +7,30 @@ class Profile extends React.Component {
         this.state = {
             count:0,
             userInfo:{
-                name:"Hello"
             }
         }
     }
     async componentDidMount() {
         const data = await fetch("https://api.github.com/users/RahulRanjan7201")
         const json  = await data.json();
+        console.log(json)
         this.setState({
             userInfo:json
         })
         console.log("Compo0nent did mount")
     }
+    componentDidUpdate() {
+        console.log("Component did update ")
+    }
     render() {
         console.log("Render")
         return (
             <>
-            <h1>Profile Class Component {this.props.name} {this.state.userInfo.name}</h1>
-            <button onClick={() => {
-                this.setState({
-                    count:1
-                })
-            }}>Click</button>
+            <h1>Profile</h1>
+            <img src={this.state.userInfo.avatar_url}/>
+            <h2>Name : {this.state.userInfo.name}</h2>
+            <h2>Bio : {this.state.userInfo.bio}</h2>
+            <h2>Location : {this.state.userInfo.location}</h2>
             </>
         )
     }
